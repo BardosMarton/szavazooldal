e80fdad64173cd3deca40d25082daa1d79793c5d
BardosMarton <bardos.marton.18f@szlgbp.hu>
2021-06-03 11:44:18 +0200

    5th (database)

--- nodeserver.js
+++ nodeserver.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config.json');
+
+mongoose.connect(config.databaseurl);
 
 const app = express();
 
@@ -8,10 +12,35 @@ app.use(express.static('public'));
     response.send(index);
 });*/
 
+const schema = new mongoose.Schema({
+    nev: String,
+    szavazatok: Number
+});
+
+const model = mongoose.model('Opciok', schema, 'Opciok');
+
 app.use(express.urlencoded());
 
 app.post("/szavazas", function(request, response) {
+    
     console.log(request.body);
+
+    model.findOne({nev: request.body.osszeg}, function(err, doc) {
+        if (doc) {
+            console.log(request.body.osszeg + ' már létezik');
+
+            doc.szavazatok++;
+            doc.save();
+        }
+        else {
+            console.log(request.body.osszeg + ' még nem létezik');
+            new model({
+                nev: request.body.osszeg,
+                szavazatok: 1
+            }).save();
+        }
+    });
+    
     response.redirect('/');
 });
 
